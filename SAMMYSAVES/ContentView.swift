3a0c906034ae2516739e0ad7a6a2821b6f15fc44
Andrea Tongsak <atongsak@gmail.com>
2020-02-08 11:14:28 -0500

    Initial Commit

---
+++ SAMMYSAVES/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SAMMYSAVES
+//
+//  Created by Andrea Tongsak on 2/8/20.
+//  Copyright © 2020 Andrea Tongsak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
